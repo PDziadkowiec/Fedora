64b7368d473ced70bdd3992bb015f9b5b34eb004
katarzynadule <68820623+katarzynadule@users.noreply.github.com>
2021-11-29 20:11:37 +0100

    Dodanie skryptu Menu

---
+++ Fedora1.0/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas canvasMainMenu, canvasSettings;
+
+    public void showSettings()
+    {
+        canvasMainMenu.enabled = false;
+        canvasSettings.enabled = true;
+    }
+    public void showMenu()
+    {
+        canvasMainMenu.enabled = true;
+        canvasSettings.enabled = false;
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
