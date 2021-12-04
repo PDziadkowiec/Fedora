8b1e9a250c9e93e27c79a87945babe622cca13f7
nifumi <alka101@tlen.pl>
2021-12-04 12:18:34 +0100

    plansza "prowincja", przejście na plansze
    dorobić (odpowiednie) wracanie przy przejściu z prowincji do lasu

---
+++ Fedora1.0/Assets/Scripts/MoveBetScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveBetScenes : MonoBehaviour
+{
+    [SerializeField] private string nextLevel;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
