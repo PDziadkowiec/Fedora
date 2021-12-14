77ec0126d9a5c579206f0950a6b919a4bf0bad6e
nifumi <alka101@tlen.pl>
2021-12-14 14:32:07 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/PDziadkowiec/Fedora""
    This reverts commit be1a4b08bfd632812c15e1080f3f91e9af5a8361.

--- Fedora1.0/Assets/Scripts/GameData.cs
+++ Fedora1.0/Assets/Scripts/GameData.cs
@@ -9,6 +9,7 @@ public static class GameData
     public static int coins = 0;
     public static int maxCoins = 100;
     public static bool jump = false;
+    public static bool grapple = false;
     public static bool triggerLadySnail = false;
     public static bool firstForestLadySnailDialogue = true;
     public static string location = "Las";
