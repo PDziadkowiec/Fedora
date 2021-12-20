492ebab3567935a6d93072cb8f5246dd46613646
WiktoriaG99 <wiktoriagajda99@gmail.com>
2021-12-20 11:00:31 +0100

    Dodanie Menu Pauzy

---
+++ Fedora1.0/Assets/Scripts/HUDMenuPauseScripts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HUDMenuPauseScripts : MonoBehaviour
+{
+    //Skrypt przypisany do obiektu GAME PAUSE HUD SCRIPTS
+    public Canvas CanvasMenuPause;
+
+    public void BackToGame()
+    {
+        CanvasMenuPause.enabled = false;
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Fedora1.0/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPause : MonoBehaviour
+{
+    //Skrypt przypisany do obiektu PLAYER
+
+    public Canvas CanvasMenuPause;
+
+    void Update()
+    {
+        MenuPauseCanvas();
+    }
+
+    void MenuPauseCanvas()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CanvasMenuPause.enabled = true;
+        }
+    }
+
+}
