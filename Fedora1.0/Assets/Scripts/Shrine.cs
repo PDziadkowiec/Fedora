51842a9ba0f3017291deb4d34e426e89034b5440
WiktoriaG99 <wiktoriagajda99@gmail.com>
2022-01-17 11:45:33 +0100

    Dodanie kapliczki (leczenie), poprawienie Order Layer czarnego tła, platform, interfejsu
    Kapliczce trzeba jeszcze dorobić zapisywanie stanu gry

---
+++ Fedora1.0/Assets/Scripts/Shrine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shrine : MonoBehaviour
+{
+    public Text PressToHealSave;
+    public Text NumberOfHealth;
+    public AudioSource audioSource;
+    public AudioClip HealthUpSE;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Pokażą się napisy co zrobić aby się wyleczyć/zapisać
+            PressToHealSave.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Jeśli naciśnięto klawisz odpowiedzialny za uleczenie się
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                GameData.healthPoints = GameData.maxHealthPoints;
+                NumberOfHealth.text = (GameData.healthPoints).ToString() + " / " + (GameData.maxHealthPoints).ToString();
+                audioSource.GetComponent<AudioSource>().PlayOneShot(HealthUpSE);
+            }
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //zapis stanu gry
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Znikną napisy co zrobić aby się wyleczyć/zapisać
+            PressToHealSave.gameObject.SetActive(false);
+
+        }
+    }
+}
