2cdf7f46738c78258a120464d5923f14d62d4ccd
WiktoriaG99 <wiktoriagajda99@gmail.com>
2022-01-17 19:35:52 +0100

    Game Over Menu - działające opcje Punktu Kontrolnego i zaczęcia gry od nowa

--- Fedora1.0/Assets/Scripts/HUDGameOverScripts.cs
+++ Fedora1.0/Assets/Scripts/HUDGameOverScripts.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,11 +11,72 @@ public class HUDGameOverScripts : MonoBehaviour
 
     Canvas gameOverHud;
 
-    public void startLevelAllOverAgain()
+    public void loadCheckpoint()
     {
-        //Poprawić
-        //Zmienić na dynamiczne wybierane aktualnej sceny?
-        SceneManager.LoadScene(1);
+        if (PlayerPrefs.GetInt("sceneIndex") > 0 && PlayerPrefs.GetInt("sceneIndex") <= 4)
+        {
+            SceneManager.LoadScene(PlayerPrefs.GetInt("sceneIndex"));
+
+            GameData.healthPoints = PlayerPrefs.GetInt("healthPoints");
+            GameData.maxHealthPoints = PlayerPrefs.GetInt("maxHealthPoints");
+            GameData.coins = PlayerPrefs.GetInt("coins");
+            GameData.maxCoins = PlayerPrefs.GetInt("maxCoins");
+            GameData.jump = Convert.ToBoolean(PlayerPrefs.GetInt("jump"));
+            GameData.grapple = Convert.ToBoolean(PlayerPrefs.GetInt("grapple"));
+            GameData.swimming = Convert.ToBoolean(PlayerPrefs.GetInt("swimming"));
+            GameData.hasGrape = Convert.ToBoolean(PlayerPrefs.GetInt("hasGrape"));
+            GameData.hasBasil = Convert.ToBoolean(PlayerPrefs.GetInt("hasBasil"));
+            GameData.hasWater = Convert.ToBoolean(PlayerPrefs.GetInt("hasWater"));
+            GameData.hasCrystal = Convert.ToBoolean(PlayerPrefs.GetInt("hasCrystal"));
+            GameData.location = PlayerPrefs.GetString("location");
+            GameData.firstForestLadySnailDialogue = Convert.ToBoolean(PlayerPrefs.GetInt("firstForestLadySnailDialogue"));
+
+            Time.timeScale = 1;
+        }
+        else
+        {
+            //Brak zapisanego stanu gry
+        }
+    }
+
+    public void startGameAllOverAgain()
+    {
+        PlayerPrefs.SetInt("healthPoints", 3);
+        PlayerPrefs.SetInt("maxHealthPoints", 3);
+        PlayerPrefs.SetInt("coins", 0);
+        PlayerPrefs.SetInt("maxCoins", 100);
+
+        PlayerPrefs.SetInt("jump", 0);
+        PlayerPrefs.SetInt("grapple", 0);
+        PlayerPrefs.SetInt("swimming", 0);
+
+        PlayerPrefs.SetInt("hasGrape", 0);
+        PlayerPrefs.SetInt("hasBasil", 0);
+        PlayerPrefs.SetInt("hasWater", 0);
+        PlayerPrefs.SetInt("hasCrystal", 0);
+
+        PlayerPrefs.SetString("location", "Las");
+        PlayerPrefs.SetInt("sceneIndex", 1);
+
+        PlayerPrefs.SetInt("firstForestLadySnailDialogue", 1);
+
+        SceneManager.LoadScene(PlayerPrefs.GetInt("sceneIndex"));
+
+        GameData.healthPoints = PlayerPrefs.GetInt("healthPoints");
+        GameData.maxHealthPoints = PlayerPrefs.GetInt("maxHealthPoints");
+        GameData.coins = PlayerPrefs.GetInt("coins");
+        GameData.maxCoins = PlayerPrefs.GetInt("maxCoins");
+        GameData.jump = Convert.ToBoolean(PlayerPrefs.GetInt("jump"));
+        GameData.grapple = Convert.ToBoolean(PlayerPrefs.GetInt("grapple"));
+        GameData.swimming = Convert.ToBoolean(PlayerPrefs.GetInt("swimming"));
+        GameData.hasGrape = Convert.ToBoolean(PlayerPrefs.GetInt("hasGrape"));
+        GameData.hasBasil = Convert.ToBoolean(PlayerPrefs.GetInt("hasBasil"));
+        GameData.hasWater = Convert.ToBoolean(PlayerPrefs.GetInt("hasWater"));
+        GameData.hasCrystal = Convert.ToBoolean(PlayerPrefs.GetInt("hasCrystal"));
+        GameData.location = PlayerPrefs.GetString("location");
+        GameData.firstForestLadySnailDialogue = Convert.ToBoolean(PlayerPrefs.GetInt("firstForestLadySnailDialogue"));
+
+        Time.timeScale = 1;
     }
     public void MenuExit()
     {
